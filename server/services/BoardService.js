f5b59bd99e59f9d770fc179e4d5ef44a1239905f
reedm26 <reedm26@yahoo.com>
2020-01-15 10:53:18 -0700

    fixed event functions

--- server/services/BoardService.js
+++ server/services/BoardService.js
@@ -1,29 +1,33 @@
-import mongoose from "mongoose"
-import Board from "../models/Board"
-import ApiError from "../utils/ApiError"
+import mongoose from "mongoose";
+import Board from "../models/Board";
+import ApiError from "../utils/ApiError";
 
-const _repository = mongoose.model('Board', Board)
+const _repository = mongoose.model("Board", Board);
 
 class BoardService {
   async getAll(userId) {
-    return await _repository.find({ authorId: userId })
+    return await _repository.find({ authorId: userId });
   }
 
   async getById(id, userId) {
-    let data = await _repository.findOne({ _id: id, authorId: userId })
+    let data = await _repository.findOne({ _id: id, authorId: userId });
     if (!data) {
-      throw new ApiError("Invalid ID or you do not own this board", 400)
+      throw new ApiError("Invalid ID or you do not own this board", 400);
     }
-    return data
+    return data;
   }
 
   async create(rawData) {
-    let data = await _repository.create(rawData)
-    return data
+    let data = await _repository.create(rawData);
+    return data;
   }
 
   async edit(id, userId, update) {
-    let data = await _repository.findOneAndUpdate({ _id: id, authorId: userId }, update, { new: true })
+    let data = await _repository.findOneAndUpdate(
+      { _id: id, authorId: userId },
+      update,
+      { new: true }
+    );
     if (!data) {
       throw new ApiError("Invalid ID or you do not own this board", 400);
     }
@@ -31,14 +35,15 @@ class BoardService {
   }
 
   async delete(id, userId) {
-    let data = await _repository.findOneAndRemove({ _id: id, authorId: userId });
+    let data = await _repository.findOneAndRemove({
+      _id: id,
+      authorId: userId
+    });
     if (!data) {
       throw new ApiError("Invalid ID or you do not own this board", 400);
     }
   }
-
 }
 
-
-const _boardService = new BoardService()
-export default _boardService
+const _boardService = new BoardService();
+export default _boardService;
