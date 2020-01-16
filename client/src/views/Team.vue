47999581abd722a6522239fbc7187c5806c6c7d9
mikedilulo <mikedilulo2019@gmail.com>
2020-01-16 16:32:23 -0700

    centers up team heading with lists underneath

--- client/src/views/Team.vue
+++ client/src/views/Team.vue
@@ -4,14 +4,14 @@
       <div class="col-md-12">
         <navbar />
       </div>
-      <div class="row container-fluid mt-5 ml-5">
-        <div class="col-md-9 d-flex justify-content-start">
+      <div class="row container-fluid mt-5 ml-5 d-flex justify-content-center">
+        <div class="col-md-9">
           <h1>TEAM ROSTER</h1>
         </div>
       </div>
       <div class="row container-fluid">
-        <div class="col-md-5 ml-5">
-          <ul v-for="team in teams" :key="team._id" class="d-flex justify-content-start">
+        <div class="col-md-12 ml-5 d-flex mt-4">
+          <ul v-for="team in teams" :key="team._id">
             <li>{{team.title}}</li>
           </ul>
         </div>
