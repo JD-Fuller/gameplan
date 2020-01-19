6bd7d39afe9f9cf60e20d40f647c1232665939cc
mikedilulo <mikedilulo2019@gmail.com>
2020-01-19 10:32:24 -0700

    routing works with login and registration page

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,174 +1,107 @@
 <template>
   <div class="login row container-fluid pr-0 pl-0">
     <div class="col-12 pr-0 pl-0">
-      <navbar/>
-    
-    <div
-    class="login row-container d-flex align-items-center justify-content-center login-background"
-  >
-      
-    <div class="col-lg-4 login-form pr-0 ml-4">
-      <!-- Default form login -->
-      <form
-        v-if="loginForm"
-        @submit.prevent="loginUser"
-        class="text-center border border-light p-5 form-for-login"
+      <navbar />
+
+      <div
+        class="login row-container d-flex align-items-center justify-content-center login-background"
       >
-        <p class="h4 mb-4 sign-login">Sign in</p>
+        <div class="col-lg-4 login-form pr-0 ml-4">
+          <!-- Default form login -->
+          <form
+            @submit.prevent="loginUser"
+            class="text-center border border-light p-5 form-for-login"
+          >
+            <p class="h4 mb-4 sign-login">Sign in</p>
 
-        <!-- Email -->
-        <input
-          type="email"
-          v-model="creds.email"
-          id="defaultLoginFormEmail"
-          class="form-control mb-4"
-          placeholder="E-mail"
-        />
+            <!-- Email -->
+            <input
+              type="email"
+              v-model="creds.email"
+              id="defaultLoginFormEmail"
+              class="form-control mb-4"
+              placeholder="E-mail"
+            />
 
-        <!-- Password -->
-        <input
-          type="password"
-          v-model="creds.password"
-          id="defaultLoginFormPassword"
-          class="form-control mb-4"
-          placeholder="Password"
-        />
+            <!-- Password -->
+            <input
+              type="password"
+              v-model="creds.password"
+              id="defaultLoginFormPassword"
+              class="form-control mb-4"
+              placeholder="Password"
+            />
 
-        <div class="d-flex justify-content-around">
-          <div>
-            <!-- Remember me -->
-            <div class="custom-control custom-checkbox">
-              <input type="checkbox" class="custom-control-input" id="defaultLoginFormRemember" />
-              <label class="custom-control-label" for="defaultLoginFormRemember">Remember me</label>
+            <div class="d-flex justify-content-around">
+              <div>
+                <!-- Remember me -->
+                <div class="custom-control custom-checkbox">
+                  <input
+                    type="checkbox"
+                    class="custom-control-input"
+                    id="defaultLoginFormRemember"
+                  />
+                  <label
+                    class="custom-control-label"
+                    for="defaultLoginFormRemember"
+                    >Remember me</label
+                  >
+                </div>
+              </div>
+              <div>
+                <!-- Forgot password -->
+                <p class="text-danger">Forgot password?</p>
+              </div>
             </div>
-          </div>
-          <div>
-            <!-- Forgot password -->
-            <p class="text-danger">Forgot password?</p>
-          </div>
-        </div>
 
-        <!-- Sign in button -->
-        <button class="btn btn-danger btn-block my-4" type="submit">Sign in</button>
+            <!-- Sign in button -->
+            <button class="btn btn-danger btn-block my-4" type="submit">
+              Sign in
+            </button>
 
-        <!-- Register -->
-        <div>
-          <div class="action d-lg-block" @click="loginForm = !loginForm">
-            <p v-if="loginForm">
-              Not a member?
-              <a class="text-danger">Register</a>
-            </p>
-          </div>
+            <!-- Register -->
+            <div>
+              <div>
+                <p>
+                  Dont have an account? Click
+                  <router-link to="/register"> here to register </router-link>
+                </p>
+              </div>
+              <!-- Social login -->
+              <p class="login-link d-lg-block d-none">or sign in with:</p>
 
-          <!-- Social login -->
-          <p class="login-link d-lg-block d-none">or sign in with:</p>
-
-          <a href="https://www.facebook.com" class="mx-2" role="button">
-            <i class="fab fa-facebook-f text-primary"></i>
-          </a>
-          <a href="https://www.twitter.com" class="mx-2" role="button">
-            <i class="fab fa-twitter text-primary"></i>
-          </a>
-          <a href="https://www.linkedin.com" class="mx-2" role="button">
-            <i class="fab fa-linkedin-in text-primary"></i>
-          </a>
-          <a href="https://www.github.com" class="mx-2" role="button">
-            <i class="fab fa-github text-dark"></i>
-          </a>
-        </div>
-
-        <!-- Default form login -->
-      </form>
-
-      <form
-        v-else @submit.prevent="register"
-        class="text-center border border-light p-5 form-for-login"
-      >
-        <p class="h4 mb-4 sign-login">Register</p>
-        <input type="userName"
-        v-model="newUser.name"
-        id="defaultRegisterFormName"
-        class="form-control mb-4"
-        placeholder="User Name">
-        <!-- Email -->
-        <input
-          type="email"
-          v-model="newUser.email"
-          id="defaultLoginFormEmail"
-          class="form-control mb-4"
-          placeholder="E-mail"
-        />
-
-        <!-- Password -->
-        <input
-          type="password"
-          v-model="newUser.password"
-          id="defaultLoginFormPassword"
-          class="form-control mb-4"
-          placeholder="Password"
-        />
-
-        <div class="d-flex justify-content-around">
-          <div>
-            <!-- Remember me -->
-            <div class="custom-control custom-checkbox">
-              <input type="checkbox" class="custom-control-input" id="defaultLoginFormRemember" />
-              <label class="custom-control-label" for="defaultLoginFormRemember">Remember me</label>
+              <a href="https://www.facebook.com" class="mx-2" role="button">
+                <i class="fab fa-facebook-f text-primary"></i>
+              </a>
+              <a href="https://www.twitter.com" class="mx-2" role="button">
+                <i class="fab fa-twitter text-primary"></i>
+              </a>
+              <a href="https://www.linkedin.com" class="mx-2" role="button">
+                <i class="fab fa-linkedin-in text-primary"></i>
+              </a>
+              <a href="https://www.github.com" class="mx-2" role="button">
+                <i class="fab fa-github text-dark"></i>
+              </a>
             </div>
-          </div>
-          <div>
-            <!-- Forgot password -->
-            <p class="text-danger">Forgot password?</p>
-          </div>
+
+            <!-- Default form login -->
+          </form>
         </div>
-
-        <!-- Sign in button -->
-        <button class="btn btn-danger btn-block my-4" type="submit">Register</button>
-
-        <!-- Register -->
-        <div>
-          <div class="action d-lg-block" @click="loginForm = !loginForm">
-            <p v-if="loginForm">
-              Not a member?
-              <a class="text-danger">Register</a>
-            </p>
-          </div>
-
-          <!-- Social login -->
-          <p class="login-link d-lg-block d-none">or sign in with:</p>
-
-          <a href="https://www.facebook.com" class="mx-2" role="button">
-            <i class="fab fa-facebook-f text-primary"></i>
-          </a>
-          <a href="https://www.twitter.com" class="mx-2" role="button">
-            <i class="fab fa-twitter text-primary"></i>
-          </a>
-          <a href="https://www.linkedin.com" class="mx-2" role="button">
-            <i class="fab fa-linkedin-in text-primary"></i>
-          </a>
-          <a href="https://www.github.com" class="mx-2" role="button">
-            <i class="fab fa-github text-dark"></i>
-          </a>
+        <div class="col-lg-4 d-flex align-items-center pl-0">
+          <img
+            src="https://images.unsplash.com/photo-1557512367-660ba857c399?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80"
+            alt="Stadium with fans and shooting fireworks"
+            class="login-picture d-none d-lg-block"
+          />
         </div>
-
-        <!-- Default form login -->
-      </form>
+      </div>
     </div>
-    <div class="col-lg-4 d-flex align-items-center pl-0">
-      <img
-        src="https://images.unsplash.com/photo-1557512367-660ba857c399?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80"
-        alt="Stadium with fans and shooting fireworks"
-        class="login-picture d-none d-lg-block"
-      />
-    </div>
-  </div>
-  </div>
   </div>
 </template>
 
 <script>
-import router from "@/router/index.js"
-import navbar from "@/components/Nav.vue"
+import router from "@/router/index.js";
+import navbar from "@/components/Nav.vue";
 export default {
   name: "login",
   data() {
@@ -177,32 +110,20 @@ export default {
       creds: {
         email: "",
         password: ""
-      },
-      newUser: {
-        email: "",
-        password: "",
-        name: ""
       }
     };
   },
-  beforeCreate() {
-    if (this.$store.state.user._id) {
-      this.$router.push({ name: "boards" });
-    }
-  },
+
   methods: {
-    register() {
-      this.$store.dispatch("register", this.newUser);
-    },
     loginUser() {
       this.$store.dispatch("login", this.creds);
     }
   },
 
-  components:
-{
-navbar
-}};
+  components: {
+    navbar
+  }
+};
 </script>
 
 <style>
