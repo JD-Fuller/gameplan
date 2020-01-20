ea4ff8b604f050b9d42efd4526317a8dafae905a
JD-Fuller <jdfuller@live.com>
2020-01-20 10:01:34 -0700

    functions - need to change v-for [events] - boards page

--- client/src/views/Boards.vue
+++ client/src/views/Boards.vue
@@ -18,45 +18,10 @@
           id="accordion"
           role="tablist"
           aria-multiselectable="true"
-          v-for="date in dates"
+          v-for="(date, [i]) in dates"
           :key="date._id"
           class="justify-content-start"
         >
-          <!-- <div class="card">
-            <h5 class="card-header" role="tab" id="headingOne">
-              <a
-                data-toggle="collapse"
-                data-parent="#accordion"
-                href="#collapseOne"
-                aria-expanded="true"
-                aria-controls="collapseOne"
-                class="d-block"
-                style="color: red; text-align: left;"
-                >Event 1
-                <i class="fa fa-chevron-down float-right"></i>
-              </a>
-            </h5>
-
-            <div
-              id="collapseOne"
-              class="collapse show"
-              role="tabpanel"
-              aria-labelledby="headingOne"
-            >
-              <div class="card-body">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life
-                accusamus terry richardson ad squid. 3 wolf moon officia aute,
-                non cupidatat skateboard dolor brunch. Food truck quinoa
-                nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua
-                put a bird on it squid single-origin coffee nulla assumenda
-                shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
-                wes anderson cred nesciunt sapiente ea proident. Ad vegan
-                excepteur butcher vice lomo. Leggings occaecat craft beer
-                farm-to-table, raw denim aesthetic synth nesciunt you probably
-                haven't heard of them accusamus labore sustainable VHS.
-              </div>
-            </div>
-          </div> -->
           <div class="card">
             <h5 class="card-header" role="tab" id="heading">
               <a
