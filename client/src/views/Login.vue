87b40598344b0abc552dac311bcae4fdee904695
JD-Fuller <jdfuller@live.com>
2020-02-19 00:16:28 -0700

    minor login adjustments with style

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,50 +1,81 @@
 <template>
   <body class="login container-fluid pr-0 pl-0">
-    
-
-      <!-- <div
+    <!-- <div
         class="login row-container d-flex align-items-center justify-content-center"
       > -->
-            <!-- <iframe width="560" height="315" src="https://www.youtube.com/embed/9YffrCViTVk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
-<!-- <video autoplay loop muted role="presentation"> <source src="//videos.ctfassets.net/u860i9c783wb/2n0DUCAebiSoM74GCZyedW/d981293f74c04b945b207f20f3c87ce1/SC_HomePage_V2.mp4" type="video/mp4"> </video> -->
+    <!-- <iframe width="560" height="315" src="https://www.youtube.com/embed/9YffrCViTVk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
+    <!-- <video autoplay loop muted role="presentation"> <source src="//videos.ctfassets.net/u860i9c783wb/2n0DUCAebiSoM74GCZyedW/d981293f74c04b945b207f20f3c87ce1/SC_HomePage_V2.mp4" type="video/mp4"> </video> -->
 
-        <video autoplay loop muted id="myVideo">
-        <source src="https://media.cdn-equinox.com/website/impossiblehappens/30Sec_MASTERMix_Fit_1920x1080_20190521_WithoutTextOverlays1.mp4" type="video/mp4">
-        </video>
+    <video autoplay loop muted id="myVideo">
+      <source
+        src="https://media.cdn-equinox.com/website/impossiblehappens/30Sec_MASTERMix_Fit_1920x1080_20190521_WithoutTextOverlays1.mp4"
+        type="video/mp4"
+      />
+    </video>
 
-        <div class="content">
-          <h1 class="d-flex justify-content-center"><i class="fas fa-running mr-3"></i>GamePlan</h1>
-          <form @submit.prevent="loginUser">
-            <div class="d-flex justify-content-center">
+    <div class="content">
+      <h1
+        class="d-flex justify-content-center"
+        style="font-variant: all-small-caps; font-size: 25px;"
+      >
+        <i class="fas fa-running mr-3"></i>GamePlan
+      </h1>
+      <div class="login-form">
+        <form @submit.prevent="loginUser">
+          <div class="d-flex justify-content-center">
             <div class="md-form">
               <i class="fas fa-envelope prefix"></i>
-              <input type="email" v-model="creds.email" id="defaultLoginFormEmail" class="form-control mb-4" placeholder="E-mail">
-              <label for="defaultLoginFormEmail" data-error="wrong" data-success="right">Type your email</label>
+              <input
+                type="email"
+                v-model="creds.email"
+                id="defaultLoginFormEmail"
+                class="form-control mb-4"
+                placeholder="E-mail"
+              />
+              <label
+                for="defaultLoginFormEmail"
+                data-error="wrong"
+                data-success="right"
+                >Enter your email</label
+              >
             </div>
-            </div>
-            <div class="d-flex justify-content-center">
-            <div class="md-form"> 
+          </div>
+          <div class="d-flex justify-content-center">
+            <div class="md-form">
               <i class="fas fa-lock prefix"></i>
-              <input type="password" v-model="creds.password" id="defaultLoginFormPassword" class="form-control mb-4" placeholder="Password">
-              <label for="defaultLoginFormPassword" data-error="wrong" data-success="right">Type your password</label>
-            <button class="btn btn-danger my-4 d-flex justify-content-center" type="submit">
-              Sign in
-            </button>
+              <input
+                type="password"
+                v-model="creds.password"
+                id="defaultLoginFormPassword"
+                class="form-control mb-4"
+                placeholder="Password"
+              />
+              <label
+                for="defaultLoginFormPassword"
+                data-error="wrong"
+                data-success="right"
+                >Enter your password</label
+              >
+              <button
+                class="btn btn-danger my-4 d-flex justify-content-center"
+                type="submit"
+              >
+                Sign in
+              </button>
             </div>
-            </div>
-          </form>        
-        </div>
-        
-        <!-- <div class="col-lg-4 d-flex align-items-center pl-0"> -->
-          <!-- <img
+          </div>
+        </form>
+      </div>
+    </div>
+
+    <!-- <div class="col-lg-4 d-flex align-items-center pl-0"> -->
+    <!-- <img
             src="https://images.unsplash.com/photo-1557512367-660ba857c399?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80"
             alt="Stadium with fans and shooting fireworks"
             class="login-picture d-none d-lg-block"
           />   -->
-        <!-- </div> -->
-      <!-- </div> -->
-    
-    
+    <!-- </div> -->
+    <!-- </div> -->
   </body>
 </template>
 
@@ -97,7 +128,7 @@ min-height: 200px;
 /* /* .action {
   cursor: pointer;
 } */
- /* .login-background { 
+/* .login-background { 
   background-color: #696969;
   background-size: cover;
   background-repeat: no-repeat;
@@ -174,11 +205,11 @@ body {
   color: black;
 }
 
- #myVideo {
+#myVideo {
   position: fixed;
   right: 0;
   bottom: 0;
-  min-width: 100%; 
+  min-width: 100%;
   min-height: 100%;
 }
 </style>
